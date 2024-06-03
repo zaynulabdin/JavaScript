a7229a7893d99337e6518c2cad35935f3c0f70be
Zain Ul Abdin <88127508+zaynulabdin@users.noreply.github.com>
2024-06-03 09:05:46 +0000

    Conversion of Data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = "33abc";
+
+//console.log(typeof score);
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// NOTES //
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// NOTES //
+
+let isLoggedIn = 1;
+let booleeanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleeanIsLoggedIn);
+
+// NOTES //
+
+// 1 => true; 0 => false
+// "" => false
+// "zain" => true
+
+// NOTES //
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
