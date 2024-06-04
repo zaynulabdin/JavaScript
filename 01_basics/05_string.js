f4cd4e72c88211df073fd0a88b45a081843b213d
Zain Ul Abdin <88127508+zaynulabdin@users.noreply.github.com>
2024-06-04 09:51:54 +0000

    strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,19 @@
+const name = "zain";
+const repoCount = 10;
+
+// console.log(name + repoCount + " Value"); //try not to use this syntex.
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("zainlg");
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf("i"));
+
+const url = "https://zain.com/zain%20langah";
+console.log(url.replace("%20", "-"));
