dd7ab9c40a844efdc7079de63248062cb8e96198
Zain Ul Abdin <88127508+zaynulabdin@users.noreply.github.com>
2024-06-04 06:02:04 +0000

    JavaScript Comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > o);
+// console.log(null == o);
+// console.log(null >= o);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===
+
+console.log("2" === 2);
