388293a1cd8c36e5949c602533b470aad15cb165
Zain Ul Abdin <88127508+zaynulabdin@users.noreply.github.com>
2024-06-06 06:34:50 +0000

    More about functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -32,3 +32,32 @@ function sayMyName() {
   // console.log(loginUserMessage("Zain"));
   console.log(loginUserMessage());
   
+  function calculateCartPrice(...num1) {
+    return num1;
+  }
+  
+  // console.log(calculateCartPrice(200, 400, 500));
+  
+  const user = {
+    username: "Zain",
+    id: 199,
+  };
+  
+  function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.username} and ID is ${anyobject.id}`);
+  }
+  
+  // handleObject(user);
+  handleObject({
+    username: "Awais",
+    id: "110",
+  });
+  
+  const myNewArray = [200, 100, 600, 400];
+  
+  function returnSecondValue(getArray) {
+    return getArray[1];
+  }
+  
+  // console.log(returnSecondValue(myNewArray));
+  console.log(returnSecondValue([200, 400, 500, 1000]));
