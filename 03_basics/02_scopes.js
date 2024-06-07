5697cf967486beb3394dd9c860cbc7f4786974b6
Zain Ul Abdin <88127508+zaynulabdin@users.noreply.github.com>
2024-06-07 11:31:39 +0000

    Scopes in JS

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,49 @@
+// var c = 300;
+let a = 300;
+
+if (true) {
+  let a = 10;
+  const b = 20;
+  // console.log("INNER: ", a);
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one() {
+  const username = "Zain";
+  function two() {
+    const website = "youtube";
+    // console.log(username);
+  }
+  // console.log(website);
+
+  two();
+}
+
+// one();
+
+if (true) {
+  const username = "zain";
+  if (username === "zain") {
+    const website = "youtube";
+    // console.log(username + website);
+
+    // console.log(website);
+  }
+
+  // console.log(username);
+}
+
+// ++++++++++ interesting ++++++++++
+
+addOne(5);
+function addOne(num) {
+  return num + 1;
+}
+
+addTwo(5);
+const addTwo = function (num) {
+  return num + 2;
+};
