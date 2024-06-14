fe5794046e8289f14fb26da86617968e04438bdd
Zain Ul Abdin <88127508+zaynulabdin@users.noreply.github.com>
2024-06-14 08:02:11 +0000

    while & dowhile loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,33 @@
+// while loop
+
+let index = 0;
+// while (index <= 10) {
+//   console.log(`Value of index is ${index}`);
+//   index = index + 2;
+// }
+
+let myArray = ["Flash", "Batman", "Superman"];
+
+let arr = 0;
+while (arr < myArray.length) {
+  // console.log(`Value is ${myArray[arr]}`);
+  arr = arr + 1;
+}
+
+// do while loop
+
+let score = 1;
+
+do {
+  console.log(`Score is ${score}`);
+  score++;
+} while (score <= 10);
+
+// do-while loop special case:
+
+// let score = 11;
+
+// do {
+//   console.log(`Score is ${score}`);
+//   score++;
+// } while (score <= 10);
